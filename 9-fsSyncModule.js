827fd02d101701704780eab399ea121de810b6b8
Harshit27473 <harshitduhan2004@gmail.com>
2026-02-24 15:12:10 +0530

    modules in node

---
+++ 9-fsSyncModule.js
@@ -0,0 +1,13 @@
+const { readFileSync, writeFileSync } = require("fs");
+console.log("start")
+const first = readFileSync("./content/first.txt", "utf-8");
+const second = readFileSync("./content/second.txt", "utf-8");
+console.log(first, second);
+
+writeFileSync(
+    './content/result.txt',
+     `the result is : ${first},${second}`,
+      { flag: 'a' }
+    )
+console.log('done with this task')
+console.log('starting the next one')
